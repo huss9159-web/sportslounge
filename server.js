7f05e68b03df7b4318b26a0bc7b09d662bf3ea10
Hussain Shabbir <hussain@abweb.com.au>
2025-11-05 17:27:14 +0500

    Fix logo

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const __dirname = path.dirname(__filename);
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use(express.json());
